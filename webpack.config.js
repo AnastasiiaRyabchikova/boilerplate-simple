0715c9997493eef11f35ff011f687caebaf448ff
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-04-04 12:05:13 +0300

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -17,6 +18,7 @@ module.exports = {
       filename: 'index.html', // название выходного файла
     }),
     new CleanWebpackPlugin(),
+    new ESLintPlugin(),
   ],
   module: {
     rules: [
@@ -34,5 +36,5 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
-  }
-}
+  },
+};
